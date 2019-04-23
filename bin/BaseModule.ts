c44ddbd1f263fa4a56400982fa5f6e8813420170
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-04-23 17:01:02 -0300

    master - adicionando objeto a ser exportado ao arquivo principal do projeto

---
+++ bin/BaseModule.ts
@@ -0,0 +1,9 @@
+import { BaseController } from '../lib/BaseController';
+import { BaseMongoRepository } from '../lib/BaseMongoRepository';
+import { BaseMongoService } from '../lib/BaseMongoService';
+import { BaseRepository } from '../lib/BaseRepository';
+import { BaseService } from '../lib/BaseService';
+import Handlers from '../lib/Handlers';
+import Page from '../lib/Page';
+
+export default { BaseController, BaseMongoRepository, BaseMongoService, BaseRepository, BaseService, Handlers, Page }
