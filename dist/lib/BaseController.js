87d2bc1433e6831db2edebb0f39b62b554a987c0
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-04-24 14:49:23 -0300

    master - corrigindo chamada aos métodos do Service feitas pelo Controller

--- dist/lib/BaseController.js
+++ dist/lib/BaseController.js
@@ -5,35 +5,25 @@ const Handlers_1 = require("./Handlers");
 class BaseController {
     constructor(service) {
         this.service = service;
-        this.get = (req, res) => {
-            this.service.get(req.params)
+        this.find = (req, res) => {
+            this.service.find(req.params)
                 .then(_.partial(Handlers_1.default.onSuccess, res))
                 .catch(_.partial(Handlers_1.default.onError, res, "Erro ao buscar dados"));
         };
-        this.getOne = (req, res) => {
-            this.service.getOne(req.params)
+        this.findById = (req, res) => {
+            this.service.findById(req.params.id)
                 .then(_.partial(Handlers_1.default.onSuccess, res))
                 .catch(_.partial(Handlers_1.default.onError, res, "Erro ao buscar dados"));
         };
-        this.getById = (req, res) => {
-            this.service.getById(req.params.id)
+        this.findAll = (req, res) => {
+            this.service.findAll(req.params.pagina, req.params.limite)
                 .then(_.partial(Handlers_1.default.onSuccess, res))
                 .catch(_.partial(Handlers_1.default.onError, res, "Erro ao buscar dados"));
         };
-        this.getAll = (req, res) => {
-            this.service.getAll()
-                .then(_.partial(Handlers_1.default.onSuccess, res))
-                .catch(_.partial(Handlers_1.default.onError, res, "Erro ao buscar dados"));
-        };
-        this.create = (req, res) => {
-            this.service.create(req.body)
+        this.save = (req, res) => {
+            this.service.save(req.body)
                 .then(_.partial(Handlers_1.default.onSuccess, res))
                 .catch(_.partial(Handlers_1.default.onError, res, "Erro salvar dados"));
-        };
-        this.update = (req, res) => {
-            this.service.update(req.body)
-                .then(_.partial(Handlers_1.default.onSuccess, res))
-                .catch(_.partial(Handlers_1.default.onError, res, "Erro atualizar dados"));
         };
         this.delete = (req, res) => {
             try {
