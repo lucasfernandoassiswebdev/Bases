e56f8ed9799a13aed0fd77ccf3856d648eb51f87
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-04-29 15:18:42 -0300

    master - documentando clase Rotas

--- lib/Rotas.ts
+++ lib/Rotas.ts
@@ -2,6 +2,12 @@ import { Application } from 'express';
 
 class Rotas {
 
+    /**
+     * Método que percorre e inicia as rotas da API
+     * @param app <Application> (express) Aplicação onde as rotas serão mapeadas
+     * @param auth <any> Classe que irá autenticar as rotas necessárias
+     * @param rotas <any[]> Classes rotas que expõe/mapeam as rotas na APi
+     */
     public iniciarRotas = async (app: Application, auth: any, rotas: any[]) => {
         await rotas.forEach(async (rota) => {
             rota.exporRotas(app, auth);
