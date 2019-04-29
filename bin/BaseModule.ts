319409d6d3476ffc0829d43d814cc859aaf8e4cf
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-04-29 15:05:02 -0300

    master - adicionando exportação das rotas

--- bin/BaseModule.ts
+++ bin/BaseModule.ts
@@ -5,6 +5,7 @@ import Manipuladores from '../lib/Manipuladores';
 import Pagina from '../lib/Pagina';
 import { Repositorio } from '../lib/Bases/Repositorio';
 import { RepositorioMongo } from '../lib/Bases/RepositorioMongo';
+import Rotas from '../lib/Rotas';
 import { Servico } from '../lib/Bases/Servico';
 import { ServicoMongo } from '../lib/Bases/ServicoMongo';
 import TokenRotas from '../lib/Segurança/Token/TokenRotas';
@@ -15,6 +16,7 @@ export {
     Controller,
     Manipuladores,
     Pagina,
+    Rotas,
     Repositorio,
     RepositorioMongo,
     Servico,
