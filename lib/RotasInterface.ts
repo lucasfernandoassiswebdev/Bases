346c27930fc7bcae6c3b5a0b9b556db81a6a78f8
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-05-08 17:17:44 -0300

    master - atualizando documentação da interface

--- lib/RotasInterface.ts
+++ lib/RotasInterface.ts
@@ -1,6 +1,11 @@
 import { Application } from 'express';
 
 export default interface RotasInterface {
+    /**
+     * Interface para as classes que irão expor as rotas necessárias para a API
+     * @param app <Application> (express)
+     * @param aut <any> opcional, Classe que irá autenticar as rotas quando necessário 
+     */
     exporRotas(app: Application, aut?: any): void;
     exporControllers(): any[];
 }
