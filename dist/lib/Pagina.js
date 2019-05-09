0c6aadec5b9e874de9be8923658247c3e979f525
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-05-09 14:36:46 -0300

    master - .gitignore fix

--- dist/lib/Pagina.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Paginavel {
-}
-exports.Paginavel = Paginavel;
-class Pagina {
-}
-exports.Pagina = Pagina;
-exports.default = Pagina;
