c71a2dcad1f96537e9cd0895048a919e32357e6c
Lucas Fernando de Assis <lucasaassis@outlook.com>
2019-05-17 16:17:58 -0300

    master - atualizando readme

--- lib/Bases/Controller.ts
+++ lib/Bases/Controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import * as _ from 'lodash';
+import _ from 'lodash';
 import Manipuladores from '../Manipuladores';
 import Servico from './Servico';
 import Util from '../Util/Util';
